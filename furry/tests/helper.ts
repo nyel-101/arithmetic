24410eb838edaefca7259ceeb99806eef61c1c27
nyel-101 <dandevjr@gmail.com>
2025-02-22 12:19:03 +0000

    style(ui): restructure view templates

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_75076a067eec4497a43d952c2596edd2 = () => {
-  return '2026-06-14T03:38:53Z';
+const node_3e2480e9868c47788f12cc1ee54c0e24 = () => {
+  return '2025-02-22T12:19:03Z';
 };

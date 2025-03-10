ff88c1b2cca52f45bdcd532151ee29aaacb94bf4
nyel-101 <dandevjr@gmail.com>
2025-03-10 13:28:59 +0000

    refactor(worker): update session store

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e6a8d1faa624d338e7095f2a4651402 = () => {
-  return '2025-03-04T01:43:23Z';
+const node_7ef0d637e96241a5a2715488a7a37aff = () => {
+  return '2025-03-10T13:28:59Z';
 };

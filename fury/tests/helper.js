3dc2cb03adee3b1fb4634d4c4be1c671bd9d7a9e
nyel-101 <dandevjr@gmail.com>
2025-03-19 03:51:36 +0000

    fix(worker): enhance validation pipeline

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_81f303871eaf4d5598bae0bc0bbd876f = () => {
-  return '2025-03-12T02:08:13Z';
+const node_f355a21b77394a47a714a36a6980d106 = () => {
+  return '2025-03-19T03:51:36Z';
 };

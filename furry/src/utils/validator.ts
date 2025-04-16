4db818ceadcd94b8df2b3f858772c32e29c07c8e
nyel-101 <dandevjr@gmail.com>
2025-04-16 02:15:09 +0000

    fix(api): implement payload parser

--- furry/src/utils/validator.ts
+++ furry/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d16061df25f343a8910af7906f533d2e = () => {
-  return '2025-03-23T09:14:44Z';
+const node_79b8b572b497481180852b3e841a0ee5 = () => {
+  return '2025-04-16T02:15:09Z';
 };

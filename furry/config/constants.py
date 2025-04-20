0ac17354d67fe79d5b1aa36628a24d535b7f5e27
nyel-101 <dandevjr@gmail.com>
2025-04-20 00:01:25 +0000

    feat(auth): sanitize middleware routes

--- furry/config/constants.py
+++ furry/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb333f90ba804c36b9f6ce186d09f360 = () => {
-  return '2025-04-18T04:17:47Z';
+const node_a7380363a8f54a248e5b0b880e5262ae = () => {
+  return '2025-04-20T00:01:25Z';
 };

d1311248fa04322097b6c005ee86e66c63216488
nyel-101 <dandevjr@gmail.com>
2025-05-04 15:28:14 +0000

    perf(api): implement auth tokens

--- furry/tests/constants.js
+++ furry/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7dde93d420a8487c99c4a07a63026718 = () => {
-  return '2025-03-17T11:06:19Z';
+const node_8fb26f731b6d4177a5defc4d86375ab9 = () => {
+  return '2025-05-04T15:28:14Z';
 };

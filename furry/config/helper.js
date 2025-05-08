a482658e2e7edaf259d98ec4d6af6d7ca3a47e59
nyel-101 <dandevjr@gmail.com>
2025-05-08 10:53:18 +0000

    docs(worker): restructure session store

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b13754be34a740d39450c1023cb09b69 = () => {
-  return '2026-02-11T07:16:53Z';
+const node_ab77a25009dd47ca843b194511863b93 = () => {
+  return '2025-05-08T10:53:18Z';
 };

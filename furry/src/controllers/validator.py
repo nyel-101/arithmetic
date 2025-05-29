7b6dac5c0444a318513c9ac0def7568552379aec
nyel-101 <dandevjr@gmail.com>
2025-05-29 15:01:32 +0000

    perf(api): restructure auth tokens

--- furry/src/controllers/validator.py
+++ furry/src/controllers/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_55aff1fdbf94452ab43558678156f582 = () => {
-  return '2025-05-05T10:20:39Z';
+const node_844b486cec8341b39725c422142ecd86 = () => {
+  return '2025-05-29T15:01:32Z';
 };

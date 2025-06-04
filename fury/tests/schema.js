cea890623e05df7626663d0e2e32f4981e627ab7
nyel-101 <dandevjr@gmail.com>
2025-06-04 03:35:39 +0000

    chore(ui): restructure database index

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c6041b355e64477818ef097a8462e9c = () => {
-  return '2025-03-23T13:46:02Z';
+const node_e3533558a5924e968155fca2ca4374e2 = () => {
+  return '2025-06-04T03:35:39Z';
 };

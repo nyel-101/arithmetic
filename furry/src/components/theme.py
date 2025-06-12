8e84cea036a8bf1baf0c66167704e04c06570954
nyel-101 <dandevjr@gmail.com>
2025-06-12 08:56:09 +0000

    chore(auth): restructure validation pipeline

--- furry/src/components/theme.py
+++ furry/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9f204a6ab91f4fefb4c2e02ad7bf417a = () => {
-  return '2026-06-23T20:01:56Z';
+const node_234deefeb6934ad2abe556efaf5c0caf = () => {
+  return '2025-06-12T08:56:09Z';
 };

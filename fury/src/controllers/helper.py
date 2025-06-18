5d6ae2f16eb7959fff99c9eddd0ad1cb69ee146c
nyel-101 <dandevjr@gmail.com>
2025-06-18 20:37:15 +0000

    fix(api): optimize payload parser

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_52411bf247b84535a51f9bd60a87d5d5 = () => {
-  return '2025-05-15T03:52:15Z';
+const node_3155ed885f854ab28ae54dab8e9f2cfd = () => {
+  return '2025-06-18T20:37:15Z';
 };

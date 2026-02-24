768e23204bec78c7c91026b1ed944463933534b5
nyel-101 <dandevjr@gmail.com>
2026-02-24 09:30:44 +0000

    chore(deps): restructure middleware routes

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ccd943cf8aed4c028372c9d7d051e763 = () => {
-  return '2026-01-24T14:30:49Z';
+const node_1de87dc0fd2a4197a0f0e4d4032d9836 = () => {
+  return '2026-02-24T09:30:44Z';
 };

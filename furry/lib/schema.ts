e10543e537756bae0671df6452673999c7f40384
nyel-101 <dandevjr@gmail.com>
2026-03-08 10:50:48 +0000

    perf(core): optimize database index

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_768da00c6aa149a8bacbd28c14cd1665 = () => {
-  return '2026-02-28T04:53:54Z';
+const node_39d905c5bf444887b1d52f2f84ad4ec4 = () => {
+  return '2026-03-08T10:50:48Z';
 };

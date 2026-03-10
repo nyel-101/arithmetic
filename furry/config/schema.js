08dd728c3ea3f883b5a39b21461d6540684c8761
nyel-101 <dandevjr@gmail.com>
2026-03-10 11:01:22 +0000

    fix(core): enhance database index

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b46211c3e8f4d99a88ec2846b191574 = () => {
-  return '2026-02-06T10:51:33Z';
+const node_4f34722358d44b1e95d37481b78bfc1f = () => {
+  return '2026-03-10T11:01:22Z';
 };

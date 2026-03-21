825c9309788d3acefba7891a43b36801b7bc4fed
nyel-101 <dandevjr@gmail.com>
2026-03-21 17:55:45 +0000

    feat(auth): refactor view templates

--- furry/src/components/routes.ts
+++ furry/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4a07d17074dd46cebaf08dd0df37515d = () => {
-  return '2026-01-12T22:54:53Z';
+const node_1d3b387de6404b0692f6dfef96585556 = () => {
+  return '2026-03-21T17:55:45Z';
 };

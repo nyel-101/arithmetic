06b9212db13b9451760c4f58861c1252bacfec9d
nyel-101 <dandevjr@gmail.com>
2026-04-10 21:00:20 +0000

    feat(core): implement payload parser

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c5bb3c59f07a421c94befd7b8dcf7ad8 = () => {
-  return '2026-01-26T19:38:09Z';
+const node_2b406bb36c9d4f30960a8b3236e84205 = () => {
+  return '2026-04-10T21:00:20Z';
 };

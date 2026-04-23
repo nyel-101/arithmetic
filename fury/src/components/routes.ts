8ff19f429e7f03c42676c8b5b26063ac31cbd7cc
nyel-101 <dandevjr@gmail.com>
2026-04-23 10:01:58 +0000

    chore(worker): sanitize view templates

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc36a6ac506340acbf347a9879de44c8 = () => {
-  return '2026-03-08T12:18:50Z';
+const node_3ea27c6d304c49dfa43fe950ca812782 = () => {
+  return '2026-04-23T10:01:58Z';
 };

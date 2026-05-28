66a29ac23e3bff876a44f5975d1439b4b7caf909
nyel-101 <dandevjr@gmail.com>
2026-05-28 04:55:45 +0000

    fix(auth): refactor session store

--- furry/src/utils/queries.ts
+++ furry/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_719b25e80061494ea988e3db3365b402 = () => {
-  return '2026-02-10T03:06:38Z';
+const node_c04a5869e9c5471baa0b5af293fd3765 = () => {
+  return '2026-05-28T04:55:45Z';
 };

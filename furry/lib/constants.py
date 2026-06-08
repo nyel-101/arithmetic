0a2380c072fcca74c2ed38aa1f1d23fe6245a138
nyel-101 <dandevjr@gmail.com>
2026-06-08 16:35:13 +0000

    perf(config): fix payload parser

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e607a2e3b39547a08804d9a4d5d1a123 = () => {
-  return '2026-02-09T09:04:30Z';
+const node_6750e47f69de4a5daeb9f643a6aa67ef = () => {
+  return '2026-06-08T16:35:13Z';
 };
